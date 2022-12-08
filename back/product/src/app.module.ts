b52fa5ffa43f4928721d47c35f0b07ae47977682
Maxime Dev <maximedaude.dev@gmail.com>
2022-12-08 15:28:40 +0100

    add structure product

--- back/product/src/app.module.ts
+++ back/product/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: 'app',
+      password: 'pwd',
+      database: 'product',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
