94f410566ea98035d186f61249180df8aa7af301
Maxime Dev <maximedaude.dev@gmail.com>
2023-01-01 18:44:25 +0100

    remove STRIPE_API_KEY

--- back/payment/routes/payment.js
+++ back/payment/routes/payment.js
@@ -1,28 +1,28 @@
 // This is your test secret API key.
 const stripe = require("stripe")(
-  "sk_test_51L5sDXHrcd5OygkvTDqSqb2o841hw59IckZBD8WPOA4iCK9yxQoWihb6IpI1A0HWi5eVTI9FA7JnmiN6S4ztJRR300BHQAvEMu"
+  process.env.STRIPE_API_KEY
 );
 
 var express = require("express");
 var router = express.Router();
 
 router.post("/create-checkout-session", async (req, res) => {
-  // req.body = {
-  //   success_url: "http://test",
-  //   cancel_url: "http://test",
-  //   line_items: [
-  //     {
-  //       price_data: {
-  //         currency: "EUR",
-  //         product_data: {
-  //           name: "name",
-  //         },
-  //         unit_amount_decimal: 100, //1€
-  //       },
-  //       quantity: 1,
-  //     },
-  //   ],
-  // };
+  req.body = {
+    success_url: "http://test",
+    cancel_url: "http://test",
+    line_items: [
+      {
+        price_data: {
+          currency: "EUR",
+          product_data: {
+            name: "name",
+          },
+          unit_amount_decimal: 100, //1€
+        },
+        quantity: 1,
+      },
+    ],
+  };
   console.log(req.body);
   console.log(req.body.line_items);
 
