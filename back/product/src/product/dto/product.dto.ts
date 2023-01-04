086cf3ff5db8e44467ef750735b5780d7fde357a
amin <aminwastaken@gmail.com>
2023-01-04 15:23:03 +0100

    Update product fields

--- back/product/src/product/dto/product.dto.ts
+++ back/product/src/product/dto/product.dto.ts
@@ -2,6 +2,11 @@ import { PartialType } from '@nestjs/swagger';
 
 export class CreateProductDto {
   name: string;
+  price: number;
+  colors?: string[];
+  sizes?: string[];
+  description?: string;
+  images?: string[];
 }
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
--- back/product/src/product/entities/product.entity.ts
+++ back/product/src/product/entities/product.entity.ts
@@ -7,4 +7,20 @@ export class Product {
 
   @Column()
   name: string;
+
+  @Column()
+  price: number;
+
+  // colors is an array of strings that can be null
+  @Column('simple-array', { nullable: true })
+  colors: string[];
+
+  @Column('simple-array', { nullable: true })
+  sizes: string[];
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column('simple-array', { nullable: true })
+  images: string[];
 }
